42c4f1fb2c986b0e8794501ce92cfd22a753a724
崔玉宝 <1176011182@qq.com>
2019-04-28 00:19:09 +0800

    给定一个字符串，逐个翻转字符串中的每个单词

---
+++ src/main/java/com/study/lintCode/ReverseWords.java
@@ -0,0 +1,85 @@
+package com.study.lintCode;
+
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 给定一个字符串，逐个翻转字符串中的每个单词。
+ * 示例 1：
+ * <p>
+ * 输入: "the sky is blue"
+ * 输出: "blue is sky the"
+ * 示例 2：
+ * <p>
+ * 输入: "  hello world!  "
+ * 输出: "world! hello"
+ * 解释: 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+ * 示例 3：
+ * <p>
+ * 输入: "a good   example"
+ * 输出: "example good a"
+ * 解释: 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+ * <p>
+ * <p>
+ * 说明：
+ * <p>
+ * 无空格字符构成一个单词。
+ * 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+ * 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+ * User: cuiyubao
+ * Date: 2019/4/27
+ * Time: 下午 11:10
+ */
+public class ReverseWords {
+    public static void main(String[] args) {
+        System.out.println(reverseWords("a good   example"));
+    }
+
+    /**
+     * 自己的
+     *
+     * @param s
+     * @return
+     */
+    public static String reverseWords(String s) {
+        String[] splitArray = s.split(" ");
+        String[] re = new String[splitArray.length];
+        for (int i = splitArray.length - 1; i >= 0; i--) {
+            re[splitArray.length - i - 1] = splitArray[i];
+        }
+        String result = "";
+        for (String s1 : re) {
+            if (s1.length() > 0) {
+                result = result + " " + s1;
+            }
+        }
+        return result.trim();
+    }
+
+    /**
+     * 别人4ms的写法
+     *
+     * @param s
+     * @return
+     */
+    public static String reverseWords0(String s) {
+        StringBuilder builder = new StringBuilder(s.length());
+        int i = s.length() - 1;
+        while (i >= 0) {
+            while (i >= 0 && s.charAt(i) == ' ') i--;
+            if (i == -1) break;
+
+            int j = s.lastIndexOf(' ', i);
+            builder.append(s.substring(j + 1, i + 1) + " ");
+            i = j - 1;
+        }
+
+        if (builder.length() > 0) builder.deleteCharAt(builder.length() - 1);
+        return builder.toString();
+    }
+
+
+}
