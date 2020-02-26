635e2eea176586a92e2bc7670242b5445a2698d6
cuiyubao <1176011182@qq.com>
2020-02-26 23:06:39 +0800

    semaphore

---
+++ src/main/java/com/study/concurrence/semaphore/SemaphoreDemo.java
@@ -0,0 +1,52 @@
+package com.study.concurrence.semaphore;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * 描述： 演示semaphore用法
+ *
+ * @author cuiyubao
+ * @date 2020/2/17 下午 11:35
+ */
+public class SemaphoreDemo {
+
+    static Semaphore semaphore = new Semaphore(3, true);
+
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(50);
+        for (int i = 0; i < 100; i++) {
+
+            service.submit(new Task());
+
+        }
+        service.shutdown();
+
+
+    }
+
+    static class Task implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                semaphore.acquire();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "拿到了许可证");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "释放了许可证");
+            semaphore.release();
+
+
+        }
+    }
+
+
+}
