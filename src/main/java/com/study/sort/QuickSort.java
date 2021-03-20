f6acfa58f708e346e964d0cdc291c7784276af1b
cuiyubao <1176011182@qq.com>
2021-03-20 21:11:39 +0800

    快速排序

---
+++ src/main/java/com/study/sort/QuickSort.java
@@ -0,0 +1,64 @@
+package com.study.sort;
+
+/**
+ * 快速排序
+ *
+ * @author cuiyubao
+ * @date 2021/3/19/019 下午 11:51
+ */
+public class QuickSort {
+    private int[] array;
+    public QuickSort(int[] array) {
+        this.array = array;
+    }
+    public void sort() {
+        quickSort(array, 0, array.length - 1);
+    }
+    public void print() {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
+    /**
+     * 递归排序
+     * @param src
+     * @param begin
+     * @param end
+     */
+    private void quickSort(int[] src, int begin, int end) {
+        if (begin < end) {
+            int key = src[begin];
+            int i = begin;
+            int j = end;
+            while (i < j) {
+                while (i < j && src[j] > key) {
+                    j--;
+                }
+                if (i < j) {
+                    src[i] = src[j];
+                    i++;
+                }
+                while (i < j && src[i] < key) {
+                    i++;
+                }
+                if (i < j) {
+                    src[j] = src[i];
+                    j--;
+                }
+            }
+            src[i] = key;
+            quickSort(src, begin, i - 1);
+            quickSort(src, i + 1, end);
+        }
+    }
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+
+}
