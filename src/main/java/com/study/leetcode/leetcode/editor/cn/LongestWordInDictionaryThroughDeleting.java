1a9844cecdeee54ed7c9e9c16a5a753f745a14af
cuiyubao <1176011182@qq.com>
2021-11-24 17:42:32 +0800

    [524]通过删除字母匹配到字典里最长单词

--- src/main/java/com/study/leetcode/leetcode/editor/cn/LongestWordInDictionaryThroughDeleting.java
+++ src/main/java/com/study/leetcode/leetcode/editor/cn/LongestWordInDictionaryThroughDeleting.java
@@ -33,24 +33,58 @@ package com.study.leetcode.leetcode.editor.cn;
 // Related Topics 数组 双指针 字符串 排序 👍 267 👎 0
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LongestWordInDictionaryThroughDeleting {
 
     public static void main(String[] args) {
-        Solution solution=new LongestWordInDictionaryThroughDeleting().new Solution();
-
+        Solution solution = new LongestWordInDictionaryThroughDeleting().new Solution();
+        solution.findLongestWord("abce", Arrays.asList("abe", "abc"));
     }
- 
+
     //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public String findLongestWord(String s, List<String> dictionary) {
-
-
-
-         return null;
+    class Solution {
+        public String findLongestWord(String s, List<String> dictionary) {
+            dictionary.sort((String s1, String s2) -> {
+                if (s1.length() > s2.length()) {
+                    return -1;
+                } else if (s1.length() == s2.length()) {
+                    char[] cS1 = s1.toCharArray();
+                    char[] cS2 = s2.toCharArray();
+                    for (int i = 0; i < s1.length(); i++) {
+                        if (cS1[i] > cS2[i]) {
+                            return 1;
+                        } else if (cS1[i] < cS2[i]) {
+                            return -1;
+                        }
+                    }
+                } else {
+                    return 1;
+                }
+                return 0;
+            });
+            for (String dic : dictionary) {
+                char[] dicChars = dic.toCharArray();
+                char[] sChars = s.toCharArray();
+                int dicIndex = 0;
+                int sIndex = 0;
+                while (sIndex < sChars.length) {
+                    if (sChars[sIndex] == dicChars[dicIndex]) {
+                        sIndex++;
+                        dicIndex++;
+                    } else {
+                        sIndex++;
+                    }
+                    if (dicIndex == dic.length()) {
+                        return dic;
+                    }
+                }
+            }
+            return "";
+        }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
