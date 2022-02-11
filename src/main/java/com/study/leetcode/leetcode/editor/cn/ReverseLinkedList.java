bd01d0a8a2b543d988e046760f5288016fd270e1
cuiyubao <1176011182@qq.com>
2022-02-11 17:08:52 +0800

    [206]反转链表

---
+++ src/main/java/com/study/leetcode/leetcode/editor/cn/ReverseLinkedList.java
@@ -0,0 +1,105 @@
+package com.study.leetcode.leetcode.editor.cn;
+//给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+// 
+// 
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,3,4,5]
+//输出：[5,4,3,2,1]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [1,2]
+//输出：[2,1]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目范围是 [0, 5000] 
+// -5000 <= Node.val <= 5000 
+// 
+//
+// 
+//
+// 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？ 
+// 
+// 
+// Related Topics 递归 链表 👍 2268 👎 0
+
+
+public class ReverseLinkedList {
+
+    public static void main(String[] args) {
+        Solution solution = new ReverseLinkedList().new Solution();
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode reverseList(ListNode head) {
+            //前一个节点
+            ListNode pre = null;
+           //当前节点
+            ListNode now = head;
+            //下一个节点
+            ListNode next;
+            while (now != null) {
+                //存储当前节点的下一个节点
+                next = now.next;
+                //当前节点指向前一个节点
+                now.next = pre;
+                //前驱节点后移
+                pre = now;
+                //当前节点后移
+                now = next;
+            }
+            return pre;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
