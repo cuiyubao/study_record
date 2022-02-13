e69234e69c31cd4f27d24cdd1f892fe30169ac37
cuiyubao <1176011182@qq.com>
2022-02-13 17:58:26 +0800

    [103] 二叉树的锯齿形层序遍历

---
+++ src/main/java/com/study/leetcode/leetcode/editor/cn/BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,124 @@
+package com.study.leetcode.leetcode.editor.cn;
+//给你二叉树的根节点 root ，返回其节点值的 锯齿形层序遍历 。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：[[3],[20,9],[15,7]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [1]
+//输出：[[1]]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [0, 2000] 内 
+// -100 <= Node.val <= 100 
+// 
+// Related Topics 树 广度优先搜索 二叉树 👍 592 👎 0
+
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeZigzagLevelOrderTraversal {
+
+    public static void main(String[] args) {
+        Solution solution = new BinaryTreeZigzagLevelOrderTraversal().new Solution();
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+            List<List<Integer>> result = new ArrayList<>();
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+            //true时向右, false时向左
+            boolean flag = true;
+            // while 循环控制从上向下一层层遍历
+            while (!queue.isEmpty()) {
+                // 记录这一层的节点值
+                LinkedList<Integer> item = new LinkedList<>();
+                int size = queue.size();
+                // for 循环控制每一层从左向右遍历
+                for (int i = 0; i < size; i++) {
+                    TreeNode node = queue.poll();
+                    // 实现 z 字形遍历
+                    if (flag) {
+                        item.addLast(node.val);
+                    } else {
+                        item.addFirst(node.val);
+                    }
+                    if (node.left != null) {
+                        queue.add(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.add(node.right);
+                    }
+                }
+                //切换方向
+                flag = !flag;
+                result.add(item);
+            }
+            return result;
+        }
+    }
+
+    //leetcode submit region end(Prohibit modification and deletion)
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
+
+
