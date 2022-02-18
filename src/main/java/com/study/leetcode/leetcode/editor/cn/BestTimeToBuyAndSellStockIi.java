98bd62afe79ccc100a6e4aa2cac4844db2de63a6
cuiyubao <1176011182@qq.com>
2022-02-18 23:56:20 +0800

    【122】买卖股票的最佳时机二

--- src/main/java/com/study/leetcode/leetcode/editor/cn/BestTimeToBuyAndSellStockIi.java
+++ src/main/java/com/study/leetcode/leetcode/editor/cn/BestTimeToBuyAndSellStockIi.java
@@ -52,7 +52,22 @@ public class BestTimeToBuyAndSellStockIi {
  
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    public int maxProfit(int[] prices) {
+        public int maxProfit(int[] prices) {
+            int n=prices.length;
+            int[][] dp=new int[n][2];
+            dp[0][0]=0;
+            dp[0][1]=-prices[0];
+            for (int i = 1; i < prices.length; i++) {
+                dp[i][0]=Math.max(dp[i-1][0],dp[i-1][1]+prices[i]);
+                dp[i][1]=Math.max(dp[i-1][1],dp[i-1][0]-prices[i]);
+            }
+            return dp[n-1][0];
+        }
+
+
+
+
+    public int maxProfit1(int[] prices) {
         int profit=0;
         for (int i = 1; i < prices.length; i++) {
             if (prices[i]-prices[i-1]>0){
